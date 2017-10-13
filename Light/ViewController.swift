3352e0e6742d92fb76464c639224670b8fab8fa0
Andrew <korablinap@gmail.com>
2017-10-13 23:53:13 +0300

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Andrew Korablin on 13.10.17.
+//  Copyright © 2017 Andrew Korablin. All rights reserved.
+//
+
+import UIKit
+var  lightOn = true
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+      
+    }
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("On", for: .normal)
+            lightButton.setTitleColor(.black, for: .normal)
+            UIScreen.main.brightness = 1.0
+        }   else   {
+            view.backgroundColor = .black
+            lightButton.setTitle("Off", for: .normal)
+            lightButton.setTitleColor(.white, for: .normal)
+            UIScreen.main.brightness = 0.1
+        }
+        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UIScreen.main.brightness = 1.0
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
